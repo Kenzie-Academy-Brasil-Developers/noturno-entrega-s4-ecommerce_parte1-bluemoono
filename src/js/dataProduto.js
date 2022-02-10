cd33e25fe76f7b94e7bf57a24ed588a6a19b195b
bluemoono <62350635+bluemoono@users.noreply.github.com>
2022-02-10 01:06:33 -0300

    Inicio do projeto

---
+++ src/js/dataProduto.js
@@ -0,0 +1,52 @@
+const produtos = [
+    {
+        id: 1,
+        nome: 'Lightweight Jacket',
+        descricao: 'Adicione um pouco de energia ao seu guarda-roupa de inverno com esta jaqueta vibrante...',
+        secao: 'Camisetas',
+        preco: 100,
+        img: '../img/lightweight-jacket.png'
+    },
+    {
+        id: 2,
+        nome: 'Black Hat',
+        descricao: 'O gorro Next.js chegou! Esta beldade bordada tem um ajuste confortável que garante que...',
+        secao: 'Acessórios',
+        preco: 100,
+        img: '../img/black-hat.png'
+    },
+    {
+        id: 3,
+        nome: 'Mask',
+        descricao: 'Esta máscara facial durável é feita de duas camadas de tecido tratado e possui presilhas...',
+        secao: 'Acessórios',
+        preco: 40,
+        img: '../img/face-mask.png'
+    },
+    {
+        id: 4,
+        nome: 'T-Shirt',
+        descricao: 'Esta t-shirt é imprescindível no seu guarda-roupa, combinando o caimento intemporal de...',
+        secao: 'Camisetas',
+        preco: 100,
+        img: '../img/t-shirt-black.png'
+    },
+    {
+        id: 5,
+        nome: 'Short-Sleeve T-Shirt',
+        descricao: 'Agora você encontrou a camiseta básica do seu guarda-roupa. É feito de um mais grosso...',
+        secao: 'Camisetas',
+        preco: 100,
+        img: '../img/t-shirt.png'
+    },
+    {
+        id: 6,
+        nome: 'Champion Packable Jacket',
+        descricao: 'Proteja-se dos elementos com esta jaqueta embalável Champion. Esta jaqueta de poliést...',
+        secao: 'Camisetas',
+        preco: 100,
+        img: '../img/packable-jacket.png'
+    }
+]
+
+module.exports = produtos;
